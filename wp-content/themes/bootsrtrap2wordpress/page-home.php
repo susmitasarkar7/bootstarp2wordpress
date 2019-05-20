8e46904760726c3a65bb57de518060acbba5065b
arunava <arunava.31@gmail.com>
2019-05-20 19:06:45 +0530

    home page code cleaning up

--- wp-content/themes/bootsrtrap2wordpress/page-home.php
+++ wp-content/themes/bootsrtrap2wordpress/page-home.php
@@ -3,27 +3,6 @@
     Template Name: Home Page
  */
 
-// Custom field
-$prelaunch_price    		= get_post_meta(13, 'prelaunch_price', true);
-$launch_price    			= get_post_meta(13, 'launch_price', true);
-$final_price    			= get_post_meta(13, 'final_price', true);
-$course_url    				= get_post_meta(13, 'course_url', true);
-$optin_text 				= get_post_meta(13, 'optin_text', true);
-$button_text    			= get_post_meta(13, 'button_text', true);
-$optin_button_text 			= get_post_meta(13, 'optin_button_text', true);
-
-// Advanced Custom Field
-$income_feature_image		= get_field('income_feature_image');
-$income_section_title		= get_field('income_section_title');
-$income_section_description = get_field('income_section_description');
-$reason_1_title 			= get_field('reason_1_title');
-$reason_1_desc 				= get_field('reason_1_description');
-$reason_2_title 			= get_field('reason_2_title');
-$reason_2_desc 				= get_field('reason_2_description');
-
-$who_feature_image 			= get_field('who_feature_image');
-$who_section_title 			= get_field('who_section_title');
-$who_section_body 			= get_field('who_section_body');
 
 $features_section_image  	= get_field('features_section_image');
 $features_section_title  	= get_field('features_section_title');
@@ -51,127 +30,13 @@ $testimonial_title 				= get_field('testimonial_title');
 get_header();
 ?>
 
-<!-- HERO
-    ================================================== -->
-    <section id="hero" data-type="background" data-speed="5">
-    	<article>
-    		<div class="container clearfix">
-    			<div class="row">
-    			
-    				<div class="col-sm-5">
-		    			<img src="<?php bloginfo('stylesheet_directory')?>/assets/img/logo-badge.png" alt="Bootstrap to Wordpress" class="logo">
-    				</div><!-- col -->
-		    		
-		    		<div class="col-sm-7 hero-text">
-			    		<h1><?php bloginfo('name') ?></h1>
-			            <p class="lead"><?php bloginfo('description') ?></p>
-			            
-			            <div id="price-timeline">
-			            	<div class="price active">
-			            		<h4>Pre-Launch Price <small>Ends soon!</small></h4>
-			            		<span><?php echo $prelaunch_price; ?></span>
-			            	</div><!-- end price -->
-			            	<div class="price">
-			            		<h4>Launch Price <small>Coming soon!</small></h4>
-			            		<span><?php echo $launch_price; ?></span>
-			            	</div><!-- end price -->
-			            	<div class="price">
-			            		<h4>Final Price <small>Coming soon!</small></h4>
-			            		<span><?php echo $final_price; ?></span>
-			            	</div><!-- end price -->
-			            </div><!-- price-timeline -->
+	<?php get_template_part( 'template-parts/content', 'hero' ); ?>	
 
-			            <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>" role="button"><?php echo $button_text; ?></a></p>
-		    		</div><!-- col -->
-		    		
-    			</div><!-- row -->
-    		</div><!-- container -->
-    	</article>
-    </section>
+	<?php get_template_part( 'template-parts/content', 'optin' ); ?>	
+	
+    <?php get_template_part( 'template-parts/content', 'boost' ); ?>
 	
 	
-	<!-- OPT IN SECTION
-	================================================== -->
-    <section id="optin">
-		<div class="container">
-			<div class="row">
-			
-				<div class="col-sm-8">
-					<p class="lead"><?php echo $optin_text; ?></p>
-				</div><!-- end col -->
-				
-				<div class="col-sm-4">
-					<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
-						<?php echo $optin_button_text; ?>
-					</button>
-				</div><!-- end col -->
-				
-			</div><!-- row -->
-		</div><!-- container -->
-    </section><!-- optin -->
-    
-    
-    <!-- BOOST YOUR INCOME
-	================================================== -->
-	<section id="boost-income">
-		<div class="container">
-			
-			<div class="section-header">
-
-				<!-- If user uploaded an image -->
-				<?php if( !empty($income_feature_image) ) : ?>
-
-					<img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>">
-
-				<?php endif; ?>
-
-				<h2><?php echo $income_section_title; ?></h2>
-			</div><!-- section-header -->
-			
-			<p class="lead"><?php echo $income_section_description; ?></p>
-			<div class="row">
-				<div class="col-sm-6">
-					<h3><?php echo $reason_1_title; ?></h3>
-					<p><?php echo $reason_1_desc; ?></p>
-				</div><!-- end col -->
-				
-				<div class="col-sm-6">
-					<h3><?php echo $reason_2_title; ?></h3>
-					<p><?php echo $reason_2_desc; ?></p>
-				</div><!-- end col -->
-			</div><!-- row -->
-		
-		</div><!-- container -->
-	</section><!-- boost-income -->
-	
-	
-	<!-- WHO BENEFITS
-	================================================== -->
-	<section id="who-benefits">
-		<div class="container">
-			
-			<div class="section-header">
-				<!-- If user uploaded an image -->
-				<?php if( !empty($who_feature_image) ) : ?>
-
-					<img src="<?php echo $who_feature_image['url']; ?>" alt="<?php echo $who_feature_image['alt']; ?>">
-
-				<?php endif; ?>
-
-				<h2><?php echo $who_section_title; ?></h2>
-			</div><!-- section-header -->
-			
-			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2">
-				
-					<?php echo $who_section_body; ?>	
-
-				</div><!-- end col -->
-			</div><!-- row -->
-
-		</div><!-- container -->
-	</section><!-- who-benefits -->
-    
 
 	<!-- COURSE FEATURES
 	================================================== -->
