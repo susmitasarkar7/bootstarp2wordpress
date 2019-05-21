bf1102244f797d6e7ca742626523acc3f895e6e2
arunava <arunava.31@gmail.com>
2019-05-22 04:24:27 +0530

    search template is ready

--- wp-content/themes/bootsrtrap2wordpress/search.php
+++ wp-content/themes/bootsrtrap2wordpress/search.php
@@ -10,46 +10,52 @@
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'bootsrtrap2wordpress' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title">
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			/* translators: %s: search query. */
+			printf( esc_html__( 'Search Results for: %s', 'bootsrtrap2wordpress' ), '<span>' . get_search_query() . '</span>' );
+			?>
+		</h1>
+	</section>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+		<div class="container">
+			<div id="primary" class="row">
+				<main id="content" class="col-sm-8">
 
-			endwhile;
+					<?php if ( have_posts() ) : ?>
 
-			the_posts_navigation();
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
 
-		else :
+							/**
+							 * Run the loop for the search to output the results.
+							 * If you want to overload this in a child theme then include a file
+							 * called content-search.php and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'search' );
 
-			get_template_part( 'template-parts/content', 'none' );
+						endwhile;
 
-		endif;
-		?>
+						the_posts_navigation();
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+					else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif;
+					?>
+				</main><!-- #content -->
+				<!-- Sidebar-
+				====================================-->
+				<aside class="col-sm-4">
+					<?php get_sidebar(); ?>
+				</aside>
+
+			</div><!-- #primary -->
+		</div><!-- .container -->
 
 <?php
-get_sidebar();
 get_footer();
--- wp-content/themes/bootsrtrap2wordpress/template-parts/content-search.php
+++ wp-content/themes/bootsrtrap2wordpress/template-parts/content-search.php
@@ -9,27 +9,44 @@
 
 ?>
 
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			bootsrtrap2wordpress_posted_on();
-			bootsrtrap2wordpress_posted_by();
+		if ( 'post' === get_post_type() ) :
 			?>
-		</div><!-- .entry-meta -->
+			<div class="post-details">
+				<i class="fa fa-user"></i> <?php the_author(); ?>
+				<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+				<i class="fa fa-folder"></i> <?php the_category(', '); ?>
+				<i class="fa fa-tags"></i> <?php the_tags('', ', ', ''); ?>
+				
+				<div class="post-comments-badge">
+					<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+				</div><!-- post-comments-badge -->
+
+				<?php edit_post_link('Edit', '<i class="fa fa-pencil"></i>', ''); ?>
+			
+			</div><!-- post-details -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php bootsrtrap2wordpress_post_thumbnail(); ?>
+		<?php if( has_post_thumbnail() ) { //check for feature image ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+			<div class="post-image">
+				<?php the_post_thumbnail(); ?>
+			</div><!-- post-image -->
 
-	<footer class="entry-footer">
-		<?php bootsrtrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+		<?php } ?>
+
+		<div class="post-excerpt">
+			<?php the_excerpt(); ?>
+		</div><!-- post-excerpt -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
