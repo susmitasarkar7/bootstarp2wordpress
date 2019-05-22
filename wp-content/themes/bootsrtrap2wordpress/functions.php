9cc56ad0bcfe2560ea387ade1189875d8ee1515a
arunava <arunava.31@gmail.com>
2019-05-22 06:12:13 +0530

    custom widget added but modal is not showing

--- wp-content/themes/bootsrtrap2wordpress/functions.php
+++ wp-content/themes/bootsrtrap2wordpress/functions.php
@@ -113,6 +113,15 @@ function bootsrtrap2wordpress_widgets_init() {
 		'before_title'  => '<h4 class="widget-title">',
 		'after_title'   => '</h4>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'Extra Sidebar', 'bootsrtrap2wordpress' ),
+		'id'            => 'sidebar-2',
+		'description'   => esc_html__( 'Add widgets here.', 'bootsrtrap2wordpress' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
+	) );
 }
 add_action( 'widgets_init', 'bootsrtrap2wordpress_widgets_init' );
 
--- wp-content/themes/bootsrtrap2wordpress/index.php
+++ wp-content/themes/bootsrtrap2wordpress/index.php
@@ -64,6 +64,7 @@ get_header();
 			================================================== -->
 		    <aside class="col-sm-4">
 				<?php get_sidebar(); ?>
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</aside>
 
 		</div><!--primary-->
